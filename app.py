4a7578eb1152d95bfafc3f1d74be1e81afec4cd4
TTVTheAgregado <igorcauan036@gmail.com>
2025-08-29 16:44:37 -0300

    Estrutura Inicial de testes iniciada: Hello World

--- app.py
+++ app.py
@@ -3,3 +3,11 @@ from flask import Flask
 
 app = Flask(__name__)   
 
+# Definir uma rota para a página inicial e uma função que será executada ao requisitar
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == "__main__":
+    app.run(debug=True)  # Executa o servidor Flask em modo de depuração
+
